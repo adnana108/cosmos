1d721cf5941f1c42969f67b99f24759d865bbe32
Corina Morar <adnana108@yahoo.com>
2022-08-02 16:17:30 +0300

    migrate css to scss

--- design.js
+++ design.js
@@ -12,41 +12,74 @@ for (let i = 0; i < purButtons.length; i++) {
     purButtons[i].addEventListener("click", addToCard)
 };
 
-$(document).ready(function () {
-    let mainBox = $(".main_box");
-    let index = 0;
 
-    function next() {
-        let curent = $(".active");
-        let nextContainer = curent.next();
+let mainBox = $(".main_box")
+let index = 0;
 
-        if (index < mainBox.length - 1 && nextContainer) {
-            index++
-            curent.removeClass("active")
-            nextContainer.addClass("active")
-        } else {
-            index = 0
-            curent.removeClass("active")
-            curent = mainBox[0]
-            curent.classList.add("active")
-        }
+function next() {
+    let curent = $(".active");
+    let nextContainer = curent.next();
+
+    if (index < mainBox.length - 1 && nextContainer) {
+        index++
+        curent.removeClass("active")
+        nextContainer.addClass("active")
+    } else {
+        index = 0
+        curent.removeClass("active")
+        curent = mainBox[0]
+        curent.classList.add("active")
     }
+}
+
+function prev() {
+    let curent = $(".active")
+    let prevContainer = curent.prev()
+
+    if (index > 0 && prevContainer) {
+        index--
+        curent.removeClass("active")
+        prevContainer.addClass("active")
+    } else {
+        index = mainBox.length - 1
+        curent.removeClass("active")
+        curent = mainBox[index]
+        curent.classList.add("active")
+    }
+}
+$(document).ready(function () {
+
     $("#first_right_arrow").on("click", next)
 
-    function prev() {
-        let curent = $(".active")
-        let prevContainer = curent.prev()
-
-        if (index > 0 && prevContainer) {
-            index--
-            curent.removeClass("active")
-            prevContainer.addClass("active")
-        } else {
-            index = mainBox.length - 1
-            curent.removeClass("active")
-            curent = mainBox[index]
-            curent.classList.add("active")
-        }
-    }
     $("#first_left_arrow").on("click", prev)
 })
+
+
+let image = $(".hero_img")
+function sliderForPhone(e) {
+    let width = image.innerWidth()
+    let pOffset = image.offset()
+    let x = e.pageX - pOffset.left
+    if (width / 2 > x) {
+        prev()
+    } else {
+        next()
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", init())
+
+function init() {
+    let query = window.matchMedia('(max-width:768px)')
+    if (query.matches) {
+        image.on("click", sliderForPhone)
+    }
+}
+
+
+
+
+
+
+
